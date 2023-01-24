2503f5ee5a10d1d9333526e02c72f38b4b02193d
Nima Farid <nima.farid@gmail.com>
2023-01-24 10:20:58 -0800

    Update spanner.tf
    spelling, naming changes, and removing DB schema definition

--- spanner.tf
+++ spanner.tf
@@ -1,6 +1,6 @@
-resource "google_spanner_instance" "main" {
+resource "google_spanner_instance" "global-game-spanner" {
   config       = var.spanner_config.location
-  display_name = var.spanner_config.instacne_name
+  display_name = var.spanner_config.instance_name
   num_nodes    = var.spanner_config.num_nodes
 }
 
@@ -8,9 +8,5 @@ resource "google_spanner_database" "spanner-database" {
   instance                 = google_spanner_instance.main.name
   name                     = var.spanner_config.db_name
   version_retention_period = "3d"
-  ddl = [
-    "CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
-    "CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
-  ]
   deletion_protection = false
 }
