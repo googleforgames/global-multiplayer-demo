fd433ea08a5100e381df6a39b8f81265850084b2
Mark Mandel <markmandel@google.com>
2023-03-06 13:50:06 -0800

    Build Docker image for Game Server in Cloud Build
    IT'S ALIVE (and surprisingly easy to do!)
    
    This has:
    
    * Dockerfile for the Unreal Engine Dedicated game server, built with the
      Unreal development container.
    * Documentation updates on how access the Unreal development containers.
    * Create the secret for the GitHub personal access token via Terraform.
    * Cloud Build script to create the image.
    
    Closes #29

--- game/README.md
+++ game/README.md
@@ -1,3 +1,34 @@
 # Droidshooter
 
-World scale demo for Google and Google Cloud for Games
+This is the Unreal Engine 5.1.0 project for the DroidShooter game!
+
+## Local Builds
+
+## Dedicated Game Server Docker Image
+
+```shell
+docker build . -t droidshooter-server
+```
+
+To run, you will need a copy of Agones [local SDK](https://agones.dev/site/docs/guides/client-sdks/local/) toolkit.
+
+In one shell, start the local testing SDK server:
+
+```shell
+./sdk-server.linux.amd64 --local
+```
+
+Then in another shell:
+
+```shell
+docker run --rm --network=host droidshooter-server:latest
+```
+
+(In non *nix OS's where Docker is not native, you may need to do a slightly different network configuration to get the
+same experience)
+
+You now have a dedicated game server running on 127.0.0.1:7777
+
+## Client building
+
+See the top level [README.md](../README.md#game-client) game client section for full launch instructions.
---
+++ infrastructure/game-server.tf
@@ -0,0 +1,41 @@
+# Copyright 2023 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "google_secret_manager_secret" "secret_github_packages" {
+  secret_id = "github-packages"
+
+  labels = {
+    "environment" = var.resource_env_label
+  }
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "pat_1" {
+  secret  = google_secret_manager_secret.secret_github_packages.id
+  enabled = true
+
+  secret_data = var.github_pat
+}
+
+resource "google_secret_manager_secret_iam_binding" "cloud_build_binding" {
+  project   = var.project
+  secret_id = google_secret_manager_secret.secret_github_packages.id
+  role      = "roles/secretmanager.secretAccessor"
+  members = [
+    "serviceAccount:cloudbuild-cicd@${var.project}.iam.gserviceaccount.com",
+  ]
+}
