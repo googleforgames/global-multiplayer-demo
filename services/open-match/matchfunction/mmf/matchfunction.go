12db8218c433f56023e49aab0b85de419bcdc0bb
Zach Loafman <zml@google.com>
2023-03-08 00:50:16 +0000

    Add a basic match function
    * We use a MatchProfile per region so that the match function is
    evaluated for each region and all open tickets
    
    * In the match function, for a given region, we sort tickets by some
    "score" that includes latency to that region and skill and assign
    matches for that region. We assign an evaluator score based on the
    aggregated score on the ticket.
    
    * The evaluator score should let us assign ideal matches between the
    regions, so in general the latency should be a big component of that
    score.

--- services/open-match/matchfunction/mmf/matchfunction.go
+++ services/open-match/matchfunction/mmf/matchfunction.go
@@ -17,41 +17,45 @@ package mmf
 import (
 	"fmt"
 	"log"
+	"sort"
 	"time"
 
+	"google.golang.org/protobuf/types/known/anypb"
 	"open-match.dev/open-match/pkg/matchfunction"
 	"open-match.dev/open-match/pkg/pb"
 )
 
 const (
-	matchName = "basic-matchfunction"
+	matchName = "match-skill-and-latency"
 
-	ticketsPerPoolPerMatch = 4
+	ticketsPerMatch = 4
 )
 
 // Run is this match function's implementation of the gRPC call defined in api/matchfunction.proto.
 func (s *MatchFunctionService) Run(req *pb.RunRequest, stream pb.MatchFunction_RunServer) error {
-	// Fetch tickets for the pools specified in the Match Profile.
-	log.Printf("Generating proposals for function %v", req.GetProfile().GetName())
+	// Fetch tickets for the single pool specified in the Match Profile.
+	p := req.GetProfile()
+	log.Printf("Generating proposals for profile %v", p.GetName())
 
-	poolTickets, err := matchfunction.QueryPools(stream.Context(), s.queryServiceClient, req.GetProfile().GetPools())
+	tickets, err := matchfunction.QueryPool(stream.Context(), s.queryServiceClient, p.GetPools()[0])
 	if err != nil {
-		log.Printf("Failed to query tickets for the given pools, got %s", err.Error())
+		log.Printf("Failed to query tickets pool %q: %v", p.GetPools()[0], err)
 		return err
 	}
 
 	// Generate proposals.
-	proposals, err := makeMatches(req.GetProfile(), poolTickets)
+	idPrefix := fmt.Sprintf("profile-%v-time-%v", p.GetName(), time.Now().Format("2006-01-02T15:04:05.00"))
+	proposals, err := makeMatches(p.GetName(), idPrefix, tickets)
 	if err != nil {
-		log.Printf("Failed to generate matches, got %s", err.Error())
+		log.Printf("Failed to generate matches: %v", err)
 		return err
 	}
 
-	log.Printf("Streaming %v proposals to Open Match", len(proposals))
 	// Stream the generated proposals back to Open Match.
+	log.Printf("Streaming %v proposals to Open Match", len(proposals))
 	for _, proposal := range proposals {
 		if err := stream.Send(&pb.RunResponse{Proposal: proposal}); err != nil {
-			log.Printf("Failed to stream proposals to Open Match, got %s", err.Error())
+			log.Printf("Failed to stream proposals to Open Match: %v", err)
 			return err
 		}
 	}
@@ -59,37 +63,52 @@ func (s *MatchFunctionService) Run(req *pb.RunRequest, stream pb.MatchFunction_R
 	return nil
 }
 
-func makeMatches(p *pb.MatchProfile, poolTickets map[string][]*pb.Ticket) ([]*pb.Match, error) {
+// Find all matches for the given profile.
+func makeMatches(profileName, idPrefix string, tickets []*pb.Ticket) ([]*pb.Match, error) {
+	if len(tickets) < ticketsPerMatch {
+		return nil, nil
+	}
+
+	// Score each ticket and sort by score.
+	ticketScores := make(map[string]float64) // map of Ticket.Id -> fitness score
+	for _, ticket := range tickets {
+		ticketScores[ticket.Id] = score(ticket.SearchFields.DoubleArgs["skill"], ticket.SearchFields.DoubleArgs["latency-"+profileName])
+	}
+	sort.Slice(tickets, func(i, j int) bool {
+		return ticketScores[tickets[i].Id] > ticketScores[tickets[j].Id]
+	})
+
 	var matches []*pb.Match
 	count := 0
-	for {
-		insufficientTickets := false
-		matchTickets := []*pb.Ticket{}
-		for pool, tickets := range poolTickets {
-			if len(tickets) < ticketsPerPoolPerMatch {
-				// This pool is completely drained out. Stop creating matches.
-				insufficientTickets = true
-				break
-			}
+	for len(tickets) >= ticketsPerMatch {
+		matchTickets := tickets[:ticketsPerMatch]
+		tickets = tickets[ticketsPerMatch:]
 
-			// Remove the Tickets from this pool and add to the match proposal.
-			matchTickets = append(matchTickets, tickets[0:ticketsPerPoolPerMatch]...)
-			poolTickets[pool] = tickets[ticketsPerPoolPerMatch:]
+		var matchScore float64
+		for _, ticket := range matchTickets {
+			matchScore += ticketScores[ticket.Id]
 		}
 
-		if insufficientTickets {
-			break
+		eval, err := anypb.New(&pb.DefaultEvaluationCriteria{Score: matchScore})
+		if err != nil {
+			log.Printf("Failed to marshal DefaultEvaluationCriteria into anypb: %v", err)
+			return nil, fmt.Errorf("Failed to marshal DefaultEvaluationCriteria into anypb: %w", err)
 		}
 
 		matches = append(matches, &pb.Match{
-			MatchId:       fmt.Sprintf("profile-%v-time-%v-%v", p.GetName(), time.Now().Format("2006-01-02T15:04:05.00"), count),
-			MatchProfile:  p.GetName(),
+			MatchId:       fmt.Sprintf("%s-%d", idPrefix, count),
+			MatchProfile:  profileName,
 			MatchFunction: matchName,
 			Tickets:       matchTickets,
+			Extensions:    map[string]*anypb.Any{"evaluation_input": eval},
 		})
-
 		count++
 	}
-
 	return matches, nil
+}
+
+func score(skill, latency float64) float64 {
+	// skill is kill/death, latency is in milliseconds - aggregate in a way that the higher the score, the better
+	// (so we subtract latency, since lower latency is better).
+	return skill - (latency / 1000.0)
 }
