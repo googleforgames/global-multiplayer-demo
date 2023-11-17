64fcb375b44a2916446d48db2567c5f2d08584cc
Andrew Marcum <amarcum@google.com>
2023-11-17 10:23:17 -0600

    Cleaned up VM app installs & added xpra

--- infrastructure/game-client-startup.sh
+++ infrastructure/game-client-startup.sh
@@ -4,11 +4,13 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+cd /etc/apt/sources.list.d 
+wget https://raw.githubusercontent.com/Xpra-org/xpra/master/packaging/repos/bookworm/xpra.sources
+sudo apt update -y
+cd -
+
 curl -OL https://github.com/LizardByte/Sunshine/releases/download/v0.21.0/sunshine-debian-bookworm-amd64.deb
 sudo apt install -f ./sunshine-debian-bookworm-amd64.deb -y
-sudo apt install xfce4 xfce4-goodies -y
-sudo apt install tightvncserver -y
-sudo apt install dbus-x11 -y
+sudo apt install xfce4 xfce4-goodies tightvncserver dbus-x11 xpra -y
 
-sudo apt update -y
 sudo apt upgrade -y
